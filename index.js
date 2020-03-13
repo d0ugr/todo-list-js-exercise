02e641af050e02ba28e5ce884d4ea8ec380997df
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-13 12:18:58 -0400

    Refactored newTask

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-const newTask = function(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+const newTask = function(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
 };
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-const completeTask = function(taskIndex) {
-  taskComplete[taskIndex] = true;
-};
+// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// const completeTask = function(taskIndex) {
+//   taskComplete[taskIndex] = true;
+// };
 
-// Print the state of a task to the console in a nice readable way
-const logTaskState = function(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-};
+// // Print the state of a task to the console in a nice readable way
+// const logTaskState = function(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// };
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+// for now, let's just make sure we see our tasks
+console.log(tasks);
