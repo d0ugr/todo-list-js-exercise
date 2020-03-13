723ba748f8a5f1f2392a42d5c004ef1e910fd6cc
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-13 12:16:38 -0400

    Passes eslint

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const taskComplete = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+const newTask = function(title) {
   taskTitles.push(title);
   taskComplete.push(false);
-}
+};
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
+const completeTask = function(taskIndex) {
   taskComplete[taskIndex] = true;
-}
+};
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
+const logTaskState = function(taskIndex) {
   const title = taskTitles[taskIndex];
   const complete = taskComplete[taskIndex];
   console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+};
 
 // DRIVER CODE BELOW
 
